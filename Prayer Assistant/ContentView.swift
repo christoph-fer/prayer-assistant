f7f67bad5851753c7cff3e98b0088e07a94fdf03
christoph-fer <christopher.vfernandes@gmail.com>
2022-09-21 22:07:53 +0530

    Initial Commit

---
+++ Prayer Assistant/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Prayer Assistant
+//
+//  Created by Christopher Fernandes on 21/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prayer Assistant/Prayer_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prayer_AssistantApp.swift
+//  Prayer Assistant
+//
+//  Created by Christopher Fernandes on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Prayer_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
